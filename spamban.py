7ca5e063342f0d39a7117d61e350aebd5d8c9bf2
heyaaman <heyaaman2004@gmail.com>
2021-12-24 19:47:16 +0530

    Create spamban.py

---
+++ spamban.py
@@ -0,0 +1,31 @@
+import asyncio
+from pyrogram import Client as Bot, filters, idle
+from pyrogram.types import Message, User
+from config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Bot(
+    ':ban:',
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+@bot.on_message(filters.command('start'))
+async def start(bot, message):
+    text = 'Hey, I am SpamBan Bot \n\n I Can Ban a Member After Joining The group. \n\n 📖 Note - Added Member will not be banned. \n\n ⚠️Warning- My use is for personal Groups.\n\n'
+    await message.reply(text, quote=True)
+    return
+
+@bot.on_message(filters.new_chat_members)
+async def kick(bot, m: Message):
+    try:
+        await bot.kick_chat_member(m.chat.id, m.from_user.id)
+        await asyncio.sleep(2)
+        await bot.unban_chat_member(m.chat.id, m.from_user.id)
+        return
+    except Exception as e:
+        print(e)
+
+	
+bot.start()
+idle()
